6385d41dd23bad9c9fd0802cdd223a99393c3daa
Nazar <romanenko1927@gmail.com>
2019-05-22 23:56:06 +0300

    Updates

---
+++ precache-manifest.615719a3f9c43f2d66642e3c7c8c6e68.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "325be0e027bf462f3064cdd8b7529c42",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "d4c665e83455f2c89aed",
+    "url": "/myapp/static/css/2.533d620f.chunk.css"
+  },
+  {
+    "revision": "00222fd120373f0cc6bf",
+    "url": "/myapp/static/css/main.dcea6c4d.chunk.css"
+  },
+  {
+    "revision": "d4c665e83455f2c89aed",
+    "url": "/myapp/static/js/2.4615b2d1.chunk.js"
+  },
+  {
+    "revision": "00222fd120373f0cc6bf",
+    "url": "/myapp/static/js/main.5fcaa01d.chunk.js"
+  },
+  {
+    "revision": "f56cbfd09050d46037d5",
+    "url": "/myapp/static/js/runtime~main.e4f53ea3.js"
+  },
+  {
+    "revision": "62586afa8dcb41886f1b344c511d2f70",
+    "url": "/myapp/static/media/254da1.62586afa.jpeg"
+  }
+]);
